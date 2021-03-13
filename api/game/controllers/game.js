f8d95398bd0cc2fe6f4a5354e8d9b20c26d095a2
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-03-13 12:03:16 -0300

    incluindo comentarios importantes sobre a customização da controller, tanto do strapi quanto do KoaJS

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,13 +1,18 @@
 'use strict';
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#controllers)
  * to customize this controller
  */
 
 module.exports = {
+  // a variavel ctx contem todas as informações relacionadas ao request e response
+  // o strapi utiliza o KOA por baixo dos panos, então da pra verificar na documentação do KOA
+  // coisas sobre objetos derivados do request e response
+  // request: (https://koajs.com/#request)
+  // response: (https://koajs.com/#response)
   populate: (ctx) => {
-    console.log("Initialize")
-    ctx.send({ ok: true });
+    console.log('Starting to populate...');
+    ctx.send("Finished populating!");
   },
 };
