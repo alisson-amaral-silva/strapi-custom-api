9708334a6a96fceb4c739d2aa8b9c44e051582be
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-03-16 19:55:38 -0300

    populando collection developers e publishers

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -6,6 +6,22 @@
  */
 
 const axios = require('axios');
+const slugify = require('slugify');
+
+async function getGameInfo(slug) {
+  const jsdom = require('jsdom');
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector('.description');
+
+  return {
+    rating: 'FREE',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
 
 module.exports = {
   populate: async (params) => {
@@ -16,6 +32,16 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products)
+    await strapi.services.publisher.create({
+      name: products[0].publisher,
+      slug: slugify(products[0].publisher).toLowerCase(),
+    });
+
+    await strapi.services.developer.create({
+      name: products[0].developer,
+      slug: slugify(products[0].developer).toLowerCase(),
+    });
+
+    console.log(products);
   },
 };
