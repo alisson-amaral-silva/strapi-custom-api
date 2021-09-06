8968e18f2d52580b91cae61fabf37a1cfafbf879
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-06 20:54:19 -0300

    integrando o paymentIntent com o stripe

--- app/api/order/controllers/order.js
+++ app/api/order/controllers/order.js
@@ -1,9 +1,7 @@
 'use strict'
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
 
 module.exports = {
   createPaymentIntent: async (ctx) => {
@@ -38,12 +36,24 @@ module.exports = {
       return acc + game.price;
     }, 0);
 
-    if(total === 0 ){
+    if (total === 0) {
       return {
         freeGames: true,
       };
     }
 
-    return { total_in_cents: total * 100, games };
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total * 100,
+        currency: "usd",
+        metadata: { integration_check: "accept_a_payment" },
+      });
+
+      return paymentIntent;
+    } catch (err) {
+      return {
+        error: err.raw.message,
+      };
+    }
   },
 };
